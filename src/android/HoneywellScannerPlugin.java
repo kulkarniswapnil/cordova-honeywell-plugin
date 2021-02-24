3d5b4da529f7a4a64c5ce7407b76182d8ec74afa
swapnil <swapnil.kulkarni@morrisonsplc.co.uk>
2021-02-24 14:26:49 +0530

    first commit

--- src/android/HoneywellScannerPlugin.java
+++ src/android/HoneywellScannerPlugin.java
@@ -35,6 +35,37 @@ public class HoneywellScannerPlugin extends CordovaPlugin implements BarcodeRead
                 manager = aidcManager;
                 barcodeReader = manager.createBarcodeReader();
                 if(barcodeReader != null){
+                                  Map<String, Object> properties = new HashMap<String, Object>();
+                    // Set Symbologies On/Off
+                    properties.put(BarcodeReader.PROPERTY_CODE_128_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_GS1_128_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_QR_CODE_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_CODE_39_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_DATAMATRIX_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_UPC_A_ENABLE, true);
+                    properties.put(BarcodeReader.PROPERTY_EAN_13_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_EAN_8_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_AZTEC_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_CODABAR_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_INTERLEAVED_25_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_PDF_417_ENABLED, true);
+                    // Set Max Code 39 barcode length
+                    properties.put(BarcodeReader.PROPERTY_CODE_39_MAXIMUM_LENGTH, 10);
+                    // Turn on center decoding
+                    properties.put(BarcodeReader.PROPERTY_CENTER_DECODE, false);
+                    // Disable bad read response, handle in onFailureEvent
+                    properties.put(BarcodeReader.PROPERTY_NOTIFICATION_BAD_READ_ENABLED, false);
+
+                    // Also send the EAN-13 and EAN-8 check digit within the payload.
+                    properties.put(BarcodeReader.PROPERTY_EAN_13_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_EAN_8_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                    properties.put(BarcodeReader.PROPERTY_UPC_A_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+
+                    // If this is not set to true, an EAN-13 starting with 00 will have the first zero removed.
+                    properties.put(BarcodeReader.PROPERTY_UPC_A_TRANSLATE_EAN13, true);
+
+                    // Apply the settings
+                    barcodeReader.setProperties(properties);
                     barcodeReader.addBarcodeListener(HoneywellScannerPlugin.this);
                     try {
                         barcodeReader.claim();
