0be56b281f21014ddaaf48f24091a2dc6a4eb456
swapnil <swapnil.kulkarni@morrisonsplc.co.uk>
2021-02-25 12:36:47 +0530

    Pluin fail issues resolved

--- www/honeywell.js
+++ www/honeywell.js
@@ -1,8 +1,29 @@
-var exec = require('cordova/exec');
+var execute = require("cordova/exec");
 
 var honeywell = {
-    listenForScans: function(success, failure) {
-        return exec(success, failure, "HoneywellScannerPlugin", "listenForScans", []);
+    softwareTriggerStart: function () {
+        return execute(null, null, 'HoneywellScannerPlugin', 'softwareTriggerStart', []);
+    },
+    softwareTriggerStop: function () {
+        return execute(null, null, 'HoneywellScannerPlugin', 'softwareTriggerStop', []);
+    },
+    nativeListen: function (res, err) {
+        return execute(res, err, 'HoneywellScannerPlugin', 'listen', []);
+    },
+    listen: function (res, err) { // DEPRECATION WARNING: This will be removed in the next major release, use nativeListen
+        return execute(res, err, 'HoneywellScannerPlugin', 'listen', []);
+    },
+    nativeRelease: function () {
+        return execute(null, null, 'HoneywellScannerPlugin', 'release', []);
+    },
+    release: function () { // DEPRECATION WARNING: This will be removed in the next major release, use nativeRelease
+        return execute(null, null, 'HoneywellScannerPlugin', 'release', []);
+    },
+    nativeClaim: function () {
+        return execute(null, null, 'HoneywellScannerPlugin', 'claim', []);
+    },
+    claim: function () { // DEPRECATION WARNING: This will be removed in the next major release, use nativeRelease
+        return execute(null, null, 'HoneywellScannerPlugin', 'claim', []);
     }
 };
 
